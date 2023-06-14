1e7e1c111cddd84f5b5b06847d1355b2078a2d03
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-14 16:59:19 +0200

    file pages router

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,21 @@
 <template>
-  <div class="container" id="product-list">
-    <h1>{{ title }}</h1>
-    <div class="row gy-4 mb-4">
-      <div
-        class="col-sm-6 col-md-4 col-lg-3"
-        v-for="(project, index) in projects"
-        :key="projects.id">
-        <div class="card">
-            <img :src="project.image" :alt="projects.name" class="img-fluid" />
-            <div class="card-body">
-              <h6>{{ project.name }}</h6>
-            </div>
-          </div>
-        </div>
-    </div>
-    
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item">
-          <button class="page-link" @click="getData(currentPage - 1)">
-            Previous
-          </button>
-        </li>
-        <li class="page-item" v-for="n in lastPage">
-          <button class="page-link" @click="getData(n)">{{ n }}</button>
-        </li>
+  <HeaderApp/>
+  <main>
+    <router-view></router-view>
+  </main>
+  
 
-        <li class="page-item">
-          <button class="page-link" @click="getData(currentPage + 1)">
-            Next
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </div>
 </template>
 
 <script>
-import axios from "axios";
-export default {
-  name: "App",
-  data() {
-    return {
-      title: "Projects",
-      projects: [],
-      apiUrl: "http://127.0.0.1:8000/api",
-      imgBasePath: "http://127.0.0.1:8000/storage/",
-      currentPage: 1,
-      lastPage: null,
-    };
-  },
-  methods: {
-    getData(numPage) {
-      axios.get(`${this.apiUrl}/projects`, {
-          params: {
-            page: numPage,
-          },
-        })
-        .then((res) => {
-          //console.log(res);
-          this.projects = res.data.results;
-        //   this.currentPage = res.data.results.current_page;
-        //   this.lastPage = res.data.results.last_page;
-          console.log(this.projects);
-        });
-    },
-  },
-  mounted() {
-    this.getData(1);
-  },
-};
+import HeaderApp from './components/HeaderApp.vue';
+
+  export default {
+    name: 'App',
+    components: { HeaderApp },
+  }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    data-bs-target="#navbarNav" aria-controls="navbarNav" 
+    aria-expanded="false" aria-label="Toggle navigation" :disabled="disableToggler"
+      @click="toggleNavLinks">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div :class="{ 'collapse': !showNavLinks, 'navbar-collapse': true, 'text-muted': true }" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item" v-for="(link, index) in menu" :key="index">
+          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+          <router-link :to="{ name: link.routeName }" class="nav-link">
+                {{ link.label }}
+          </router-link>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderApp',
+    data(){
+      return {
+
+        showNavLinks: false,
+        disableToggler:false,
+
+        menu: [
+          {
+            label: 'Home',
+            routeName: 'home'
+          },
+          {
+            label: 'Projects',
+            routeName: 'projects'
+          },
+          {
+            label: 'About Us',
+            routeName: 'about'
+          },
+          {
+            label: 'Contacts',
+            routeName: 'contact'
+          },
+          
+        ]
+      }
+    },
+    methods: {
+      toggleNavLinks() {
+        this.showNavLinks = !this.showNavLinks;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
